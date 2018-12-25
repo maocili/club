0329e27ac2355d5864402828fd5fdcae3518295c
maocili <maocili@163.com>
2018-12-25 10:44:13 +0800

    创建Activity类

---
+++ src/main/java/com/example/club/domain/Activity.java
@@ -0,0 +1,78 @@
+package com.example.club.domain;
+
+import java.util.Date;
+
+public class Activity {
+    private int id;
+    private String activity_title;
+    private String activity_name;
+    private String activity_content;
+    private String activity_address;
+    private String username;
+    private String time;
+    private String img_src;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getActivity_title() {
+        return activity_title;
+    }
+
+    public void setActivity_title(String activity_title) {
+        this.activity_title = activity_title;
+    }
+
+    public String getActivity_name() {
+        return activity_name;
+    }
+
+    public void setActivity_name(String activity_name) {
+        this.activity_name = activity_name;
+    }
+
+    public String getActivity_content() {
+        return activity_content;
+    }
+
+    public void setActivity_content(String activity_content) {
+        this.activity_content = activity_content;
+    }
+
+    public String getActivity_address() {
+        return activity_address;
+    }
+
+    public void setActivity_address(String activity_address) {
+        this.activity_address = activity_address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getImg_src() {
+        return img_src;
+    }
+
+    public void setImg_src(String img_src) {
+        this.img_src = img_src;
+    }
+}
